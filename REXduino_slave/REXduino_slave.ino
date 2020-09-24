1164c4ee88e0282b1ab67c3df1840550aa886907
Jaroslav SOBOTA <jsobota@ntis.zcu.cz>
2020-09-24 17:09:53 +0200

    Version bumped to 2.1.1

--- REXduino_slave/REXduino_slave.ino
+++ REXduino_slave/REXduino_slave.ino
@@ -30,8 +30,8 @@
 **************************************************************************/
 
 #define MAJORVERSION 2
-#define MINORVERSION 0 //even number = release, odd number = development
-#define REVISION 3 //for hotfixes, even number = hotfix applied, odd number = development
+#define MINORVERSION 1 //even number = release, odd number = development
+#define REVISION 1 //for hotfixes, even number = hotfix applied, odd number = development
 #define COMMIT 0 //
 
 //#define USE1WIRE //uncomment this line to use 1-Wire temperature sensors, OneWire library is required
